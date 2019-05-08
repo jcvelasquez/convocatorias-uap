f2bb4568add25ff8458baf34fe69a990c7c9de4d
Jose Claudio Velasquez <jcjrvb@gmail.com>
2019-05-08 08:52:40 -0500

    con base de atoa

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -52,16 +52,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 											</ul>
 											<div class="tab-content">
 												<div class="tab-pane active" id="kt_tabs_5_1" role="tabpanel">
-													Lorem Ipsum is simxxxxxxxxply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+													
+
 												</div>
 												<div class="tab-pane" id="kt_tabs_5_2" role="tabpanel">
-													It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+													
+
 												</div>
 												<div class="tab-pane" id="kt_tabs_5_3" role="tabpanel">
-													Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged
+													
+
 												</div>
 												<div class="tab-pane" id="kt_tabs_5_4" role="tabpanel">
-													Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+													
+													
 												</div>
 											</div>
 										</div>
